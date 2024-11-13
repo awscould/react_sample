113990b27e780e11ffb64c5212212cbfcbf0a59e
Manikandan <43571823+awscould@users.noreply.github.com>
2024-11-13 19:42:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and WebApp-React.
+          Edit <code>src/App.js</code> and save to reloading.
         </p>
         <a
           className="App-link"
