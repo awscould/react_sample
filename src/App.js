07743471109cbdf3b6841758b95b7d7b1320430b
Manikandan <43571823+awscould@users.noreply.github.com>
2024-11-13 19:54:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - web
+          Learn React - web21
         </a>
       </header>
     </div>
